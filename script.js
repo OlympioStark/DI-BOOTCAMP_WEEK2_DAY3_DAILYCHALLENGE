1435795a463d0306d6b7c0f31cd83e6f603c65b0
OlympioStark <melessgilleschrist@gmail.com>
2022-12-30 07:49:29 +0000

    Daily Challenge

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-let n = "*"
-let p = "**"
-for (let i = 0; i < 1; i++) {
-    console.log(`${n}`);
-    for (let j = 0; j < 1; j++) {
-        console.log(`${p}`);
-        for (let k = 0; k < 1; k++) {
-            console.log(`${n}${p}`);
-            for (let l = 0; l < 1; l++) {
-                console.log(`${p}${p}`);
-                for (let m = 0; m < 1; m++) {
-                    console.log(`${p}${p}${n}`);
-                    for (let a = 0; a < 1; a++) {
-                        console.log(`${p}${p}${p}`);
-                        break
-                    }
-                }
-            }
-        }
+// 1.
+const n = 5
+let p = ""
+for (let i = 0; i < n; i++) {
+    p = p.concat("*")
+    console.log(`${p}`);
+}
+
+//2.
+const t = 5
+for (let j = 0; j < t; j++) {
+    let star1 = ""
+    for (let k = 0; k < j + 1; k++) {
+        star1 = star1.concat("*")
     }
+    console.log(`${star1}`);
 }
